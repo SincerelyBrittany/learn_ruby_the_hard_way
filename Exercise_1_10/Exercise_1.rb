d1b76222c9b9ec0a2242887bcc0cd0224cc20119
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-05-27 18:00:59 -0400

    added folder exercise 1-10 and added exercise 1 and 2 file

---
+++ Exercise_1_10/Exercise_1.rb
@@ -0,0 +1,18 @@
+#To run this program run 'ruby Exercise_1_10/Exercise_1.rb'
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.''
+
+#Extra Credit
+
+# For this exercise, try these things:
+# Make your script print another line.
+# Make your script print only one of the lines.
+# Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+
+puts "This is another line"
